1624a67fe48a719b872c6df3490f030bc8ba3104
Borislav Varbanov <b.vurbanov@gmail.com>
2023-01-01 10:40:30 +0000

    Optimize overall gamma curve handling

---
+++ pipeline_adjustments.py
@@ -0,0 +1,56 @@
+"""Historical tuning adjustments for the GPU pipeline examples.
+
+This module stores incremental tweaks applied while evolving the demo notebooks.
+"""
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import List, Optional
+
+
+@dataclass(frozen=True)
+class Adjustment:
+    date: str
+    label: str
+    exposure: float
+    gamma: float
+    saturation: float
+    notes: str
+
+
+ADJUSTMENTS: List[Adjustment] = [
+    Adjustment(date="2023-01-01T10:40:30Z", label="gamma-curve", exposure=1.213, gamma=2.297, saturation=0.9126, notes="Trimmed drift detected in rotating cube footage."),
+]
+
+AVERAGE_EXPOSURE = 1.2130
+AVERAGE_GAMMA = 2.2970
+AVERAGE_SATURATION = 0.9126
+
+LABEL_COUNTS = {
+    "gamma-curve": 1,
+}
+
+def latest_adjustment() -> Adjustment:
+    """Return the most recent tuning adjustment."""
+    return ADJUSTMENTS[-1]
+
+def find_adjustment(label: str) -> Optional[Adjustment]:
+    """Fetch the last adjustment recorded for a given label."""
+    for adjustment in reversed(ADJUSTMENTS):
+        if adjustment.label == label:
+            return adjustment
+    return None
+
+def recommended_settings(base_exposure: float, base_gamma: float, base_saturation: float) -> Adjustment:
+    """Blend the latest tuning values with supplied baseline settings."""
+    latest = latest_adjustment()
+    return Adjustment(
+        date=latest.date,
+        label=latest.label,
+        exposure=round(base_exposure * latest.exposure, 4),
+        gamma=round(base_gamma * latest.gamma / 2.0, 4),
+        saturation=round(base_saturation * latest.saturation, 4),
+        notes=f"Composed with {latest.label} guidance",
+    )
+
