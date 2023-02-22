89fac18a298f9b906a2d927a8b7765723b3ae26a
Borislav Varbanov <b.vurbanov@gmail.com>
2023-02-22 16:04:13 +0000

    Improve motion blend flow

--- pipeline_adjustments.py
+++ pipeline_adjustments.py
@@ -27,16 +27,17 @@ ADJUSTMENTS: List[Adjustment] = [
     Adjustment(date="2023-02-11T18:52:02Z", label="texture-balance", exposure=0.9994, gamma=1.9683, saturation=0.8369, notes="Rebalanced weights after night runs."),
     Adjustment(date="2023-02-13T08:42:47Z", label="gamma-curve", exposure=1.1784, gamma=2.5079, saturation=1.2234, notes="Smoothed spikes spotted in staging renders."),
     Adjustment(date="2023-02-14T08:06:50Z", label="luminosity-check", exposure=1.1346, gamma=2.1171, saturation=0.9162, notes="Trimmed drift detected in rotating cube footage."),
+    Adjustment(date="2023-02-22T16:04:13Z", label="motion-blend", exposure=1.2887, gamma=1.7548, saturation=1.1036, notes="Rebalanced weights after night runs."),
 ]
 
-AVERAGE_EXPOSURE = 1.0835
-AVERAGE_GAMMA = 2.1301
-AVERAGE_SATURATION = 1.0140
+AVERAGE_EXPOSURE = 1.1092
+AVERAGE_GAMMA = 2.0831
+AVERAGE_SATURATION = 1.0252
 
 LABEL_COUNTS = {
     "gamma-curve": 3,
     "luminosity-check": 2,
-    "motion-blend": 1,
+    "motion-blend": 2,
     "texture-balance": 1,
 }
 
