6e773c316acacceeaa62b4711d7ba3d756d538b4
Borislav Varbanov <b.vurbanov@gmail.com>
2023-03-18 15:37:55 +0000

    Harden gamma curve defaults

--- pipeline_adjustments.py
+++ pipeline_adjustments.py
@@ -29,14 +29,15 @@ ADJUSTMENTS: List[Adjustment] = [
     Adjustment(date="2023-02-14T08:06:50Z", label="luminosity-check", exposure=1.1346, gamma=2.1171, saturation=0.9162, notes="Trimmed drift detected in rotating cube footage."),
     Adjustment(date="2023-02-22T16:04:13Z", label="motion-blend", exposure=1.2887, gamma=1.7548, saturation=1.1036, notes="Rebalanced weights after night runs."),
     Adjustment(date="2023-03-03T17:36:07Z", label="temporal-dither", exposure=0.9963, gamma=2.1709, saturation=0.9129, notes="Mirrored adjustment from realtime demo results."),
+    Adjustment(date="2023-03-18T15:37:55Z", label="gamma-curve", exposure=0.8635, gamma=2.3417, saturation=0.8237, notes="Smoothed spikes spotted in staging renders."),
 ]
 
-AVERAGE_EXPOSURE = 1.0966
-AVERAGE_GAMMA = 2.0929
-AVERAGE_SATURATION = 1.0127
+AVERAGE_EXPOSURE = 1.0733
+AVERAGE_GAMMA = 2.1178
+AVERAGE_SATURATION = 0.9938
 
 LABEL_COUNTS = {
-    "gamma-curve": 3,
+    "gamma-curve": 4,
     "luminosity-check": 2,
     "motion-blend": 2,
     "temporal-dither": 1,
