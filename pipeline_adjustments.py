440f5461ce2baba9f93388b1c2b1894d41169331
Borislav Varbanov <b.vurbanov@gmail.com>
2023-12-09 17:23:28 +0000

    Tune exposure controller heuristics

--- pipeline_adjustments.py
+++ pipeline_adjustments.py
@@ -64,14 +64,15 @@ ADJUSTMENTS: List[Adjustment] = [
     Adjustment(date="2023-11-24T16:27:26Z", label="temporal-dither", exposure=1.152, gamma=1.7553, saturation=1.0076, notes="Collected quick QA notes for follow-up."),
     Adjustment(date="2023-11-29T09:03:44Z", label="gamma-curve", exposure=1.1397, gamma=1.8617, saturation=1.1063, notes="Smoothed spikes spotted in staging renders."),
     Adjustment(date="2023-12-05T09:04:08Z", label="motion-blend", exposure=0.9589, gamma=2.1739, saturation=0.8386, notes="Mirrored adjustment from realtime demo results."),
+    Adjustment(date="2023-12-09T17:23:28Z", label="exposure-controller", exposure=1.3139, gamma=1.9143, saturation=0.8949, notes="Smoothed spikes spotted in staging renders."),
 ]
 
-AVERAGE_EXPOSURE = 1.0864
-AVERAGE_GAMMA = 2.1216
-AVERAGE_SATURATION = 1.0225
+AVERAGE_EXPOSURE = 1.0915
+AVERAGE_GAMMA = 2.1170
+AVERAGE_SATURATION = 1.0197
 
 LABEL_COUNTS = {
-    "exposure-controller": 1,
+    "exposure-controller": 2,
     "gamma-curve": 8,
     "luminosity-check": 5,
     "motion-blend": 5,
